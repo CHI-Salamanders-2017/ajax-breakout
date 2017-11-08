ef88981d9b397f7eb97f02288b52674ea63968b2
Alyssa Diaz <alydiaz77@gmail.com>
2017-11-08 13:04:46 -0600

    breakout 1

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,9 @@ get '/' do
   @playlists = Playlist.all
   erb :index
 end
+
+get '/playlists' do
+  content_type :json
+  @playlists = Playlist.all
+  @playlists.to_json
+end
--- public/js/common.js
+++ public/js/common.js
@@ -1,2 +1,32 @@
-$(document).ready(function(){
-});
+// var playlists;
+
+$(document).ready(function() {
+  // $.ajax({
+  //   url: "/playlists"
+  // }).done(function(response){
+  //   playlists = response;
+  // });
+
+// PSEUDOCODE
+
+  // Figure out what we are selecting to bind to
+  // figure out what event you are going bind/handle
+  // setup the event handler
+  // decide if you need to prevent a default behavior
+  // check yourself - console.log()
+  // update the dom / do whatever you planned on
+
+// END PSEUDOCODE
+
+  // tr inside the tbody inside the #playlistsContainer
+  $("#playlistsContainer tbody > tr").on("click", function(event){
+    // remove the class 'info' from any row in the tbody
+    // add class 'info' to the row we clicked on
+    $(this).closest("tbody").find("tr").removeClass("info");
+    $(this).addClass("info");
+  });
+
+
+
+})
+
